14976812e57cbdce01278ae8321e095368b9c428
Joshwa Facistol <fastline2123@gmail.com>
2020-10-12 17:31:58 +0800

    Update first Video link

--- routes/auth.js
+++ routes/auth.js
@@ -68,7 +68,7 @@ router.post('/signup', async (req, res) => {
                         <br /><br />
                         <span style="color: red;">GOLDEN RULE #1 = NEVER FAKE YOUR PROFILE, FAKING OF PROFILES WILL RESULT TO BEING BANNED FROM APPLYING WITH DRAFTSOURCE VIRTUAL</span>
                         <br /><br />
-                        <span style="color: red;">Watch the video for proper instructions:</span> Watch Video<br />
+                        <span style="color: red;">Watch the video for proper instructions:</span> <a href="https://bit.ly/3lF1sX0" target="_blank">Watch Video</a><br />
                         Password: 4321
                         <br /><br />
                         <br />
