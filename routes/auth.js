d740c868807c7431c5529ed70c647e3b687771b5
Joshwa Facistol <fastline2123@gmail.com>
2020-10-12 20:15:34 +0800

    Update compose email

--- routes/auth.js
+++ routes/auth.js
@@ -63,26 +63,24 @@ router.post('/signup', async (req, res) => {
                         Someone has used your email to engage in Employment Activities, if this is your email click the link below to get started
                         <br /><br />
                         <a href="https://draftsourcevirtual.com/verify/${verificationToken}">https://draftsourcevirtual.com/verify/${verificationToken}</a>
-                        <br /><br />
+						<br /><br />
+						<span style="color: red;">WATCH THE SECOND VIDEO FOR PROPER GUIDANCE</span>
+						<br /><br />
                         <span style="color: red;">PLEASE TAKE TIME TO FOLLOW THE INSTRUCTIONS, NON COMPLIANCE WILL RESULT TO AUTOMATIC PROFILE REJECTION</span>
                         <br /><br />
                         <span style="color: red;">GOLDEN RULE #1 = NEVER FAKE YOUR PROFILE, FAKING OF PROFILES WILL RESULT TO BEING BANNED FROM APPLYING WITH DRAFTSOURCE VIRTUAL</span>
-                        <br /><br />
-                        <span style="color: red;">Watch the video for proper instructions:</span> Watch Video<br />
-                        Password: 4321
-                        <br /><br />
-                        <br />
-                        <strong>Step 2</strong><br />
+                        <br /><br /><br />
+                        <span style="font-weight: bold;">Step 2</span><br />
                         - Make a well lit, professional clear headshot<br />
                         - No half/full body shots<br />
                         - No selfies and no unprofessional headshots (This is a CV - Profile)
                         <br /><br />
-                        <strong>Step 3</strong><br />
+                        <span style="font-weight: bold;">Step 3</span><br />
                         - Upload all photos not more than 5 min<br />
                         - 8GB RAM minimum for 2D and 16 GB RAM minimum for 3D<br />
                         - Do not crop your screenshots make sure the time is included in the photo
                         <br /><br />
-                        <strong>Step 4</strong><br />
+                        <span style="font-weight: bold;">Step 4</span><br />
                         - Full-time work = 40 hours per week and Part-time work = 20 hours per week<br />
                         - You may use vocaroo.com for video recording<br />
                         - Do not say hi, hello, hey and uhm in any part of the video<br />
@@ -90,36 +88,36 @@ router.post('/signup', async (req, res) => {
                         - Do not include name, email and personal details<br />
                         - Headlines should be your core competencies
                         <br /><br />
-                        <strong>Please follow the format for the 60 sec.</strong><br />
+                        <span style="font-weight: bold;">Please follow the format for the 60 sec.</span><br />
                         Introduction <br />
-                        " I am a _______ and a _______. I have been in the industry for over _____ years and I have handled projects such as __________. My best project was completing a ________ for ________ and a ________ for _________. I am skilled at __________. + <strong>Include Final Pitch</strong>
+                        " I am a _______ and a _______. I have been in the industry for over _____ years and I have handled projects such as __________. My best project was completing a ________ for ________ and a ________ for _________. I am skilled at __________. + <span style="font-weight: bold;">Include Final Pitch</span>
                         <br /><br />
-                        <strong>Final Pitch Samples</strong><br />
+                        <span style="font-weight: bold;">Final Pitch Samples</span><br />
                         " I have experience running an Interior Design Firm of 10 people and I have generated over P100M in sales in 4 years as CEO of the company. "
                         <br /><br />
                         " I have led a team of 14 drafters while working for Australian and US companies for over 6 years " 
                         <br /><br />
                         " I was awarded as the best CAD Manager for 6 consecutive years with a US Based Engineering Company" 
                         <br /><br />
-                        <strong>Sample Voice Recordings</strong><br />
+                        <span style="font-weight: bold;">Sample Voice Recordings</span><br />
                         “I am a licensed mechanical engineer and  I have been in the industry for over 15 years and worked as a senior mechanical draftsman for 8 years for a global engineering company and I have handled multiple complex projects such as hotels, resorts, and malls. My best project was designing/drafting a 60 story residential condominium and a 2000 sqm packaging manufacturing plant. I am skilled at using Revit, AutoCAD and Quantity Surveying. I have a led a team of 14 drafters while working for Australian and US companies for over 6 years.” 
                         <br /><br />
                         “I am a furniture maker and a general contractor. I have been in the industry for over 4 years and I have handled complex interior renovation projects such as hotels, resorts, malls, restaurants and offices. My biggest accomplishment was completing the 500sqm office of Red Bull Philippines and connecting 407m of HDPE pipes via Horizontal Directional Drilling for Manila Water. I am skilled at manufacturing solid wood, outdoor weaved, kitchen cabinetry, metal works, sofas and upholstered furniture. I have experience running an interior design firm of 10 people and I have generated P100M in sales over 4 years as Founder and CEO of my company."
                         <br /><br />
-                        <strong>Sample Headlines</strong><br /><br />
-                        <strong>- Professional Civil Engineer (PE)</strong><br />
-                        <strong>- Architect Designer | Civil & Structural Engineer</strong><br />
-                        <strong>- Civil Engineer | Civil 3D & AutoCAD Specialist | CAD Designer</strong><br />
-                        <strong>- BIM & CAD Expert (Arch/Struct, MEP, Civil, Point Cloud) (US-Based)</strong><br />
-                        <strong>- MEP Design Engineer | MEP Estimator | Mechanical Engineer</strong><br />
+                        <span style="font-weight: bold;">Sample Headlines</span><br /><br />
+                        <span style="font-weight: bold;">- Professional Civil Engineer (PE)</span><br />
+                        <span style="font-weight: bold;">- Architect Designer | Civil & Structural Engineer</span><br />
+                        <span style="font-weight: bold;">- Civil Engineer | Civil 3D & AutoCAD Specialist | CAD Designer</span><br />
+                        <span style="font-weight: bold;">- BIM & CAD Expert (Arch/Struct, MEP, Civil, Point Cloud) (US-Based)</span><br />
+                        <span style="font-weight: bold;">- MEP Design Engineer | MEP Estimator | Mechanical Engineer</span><br />
                         <br />
-                        <strong>Step 5</strong><br />
+                        <span style="font-weight: bold;">Step 5</span><br />
                         - Put your most recent employment history, descending<br />
                         - Do not put irrelevant information such as Worked as a Cook for a Fastfood Restaurant, Franchised Master Siomai, Network Marketer for Usana and Front Row etc.<br />
                         -  Do not include company names in About the company instead change the names to "they" or "the company"<br />
                         - Copy from your company website or company profile<br />
                         <br /><br />
-                        <strong>Example About the Company</strong><br />
+                        <span style="font-weight: bold;">Example About the Company</span><br />
                         From<br />
                         Spaceplan Furniture & Interiors is a total interior finishing company in the Philippines.<br />
                         We offer end to end solutions from Interior Conceptualization + Design Development + Construction Management Services and Custom Furniture. We cater to Restaurants & Bars, Hotels, Offices, Residential and Hospitality companies.<br />
@@ -128,13 +126,13 @@ router.post('/signup', async (req, res) => {
                         The company is a total interior finishing company in the Philippines.<br />
                         They offer end to end solutions from Interior Conceptualization + Design Development + Construction Management Services and Custom Furniture. They cater to Restaurants & Bars, Hotels, Offices, Residential and Hospitality companies.<br />
                         <br />
-                        <strong style="color: red;">STRICT JOB DESCRIPTION FORMAT</strong><br />
-                        <strong>Example</strong><br />
+                        <span style="color: red; font-weight: bold;">STRICT JOB DESCRIPTION FORMAT</span><br />
+                        <span style="font-weight: bold;">Example</span><br />
                         • Leads all managers in the organization including construction managers, design managers, and plant managers.
                         <br /><br />
-                        <strong>Bullet + Space + Job Description</strong>
+                        <span style="font-weight: bold;">Bullet + Space + Job Description</span>
                         <br /><br />
-                        <strong>Step 6</strong><br />
+                        <span style="font-weight: bold;">Step 6</span><br />
                         - Include all Software and Skills even not included (press add or enter)<br />
                         - Include at least 3 but put all for keyword search <br />
                         - Include a minimum of 6 best projects (the more the better, the bigger the better)<br />
@@ -144,33 +142,31 @@ router.post('/signup', async (req, res) => {
                         <span style="color: red;">PLEASE TAKE TIME TO FOLLOW THE INSTRUCTIONS, NON COMPLIANCE WILL RESULT TO AUTOMATIC PROFILE REJECTION</span>
                         <br /><br />
                         <span style="color: red;">GOLDEN RULE #1 = NEVER FAKE YOUR PROFILE, FAKING OF PROFILES WILL RESULT TO BEING BANNED FROM APPLYING WITH DRAFTSOURCE VIRTUAL</span>
-                        <br /><br />
-                        <span style="color: red;">Watch the video for proper instructions:</span> Watch Video
                         <br /><br /><br />
-                        <strong>FAQ</strong>
-                        <strong>1. What are the next steps after creating a profile?</strong><br />
+                        <span style="font-weight: bold;">FAQ</span>
+                        <span style="font-weight: bold;">1. What are the next steps after creating a profile?</span><br />
                         Step 1: Profile Verification<br />
                         Step 2: Zoom Call and Pre-screening<br />
                         <br />
-                        <strong>2. Who will we be working with?</strong><br />
+                        <span style="font-weight: bold;">2. Who will we be working with?</span><br />
                         - You will be working with Australian, United States of America, United Kingdom, New Zealand and Canadian Freelancers and SMEs<br />
                         <br />
-                        <strong>3. Is there a guarantee that we will get hired if we apply?</strong><br />
+                        <span style="font-weight: bold;">3. Is there a guarantee that we will get hired if we apply?</span><br />
                         - There is no risk but there is ZERO chance if you do not apply<br />
                         <br />
-                        <strong>4. What will happen if we fake our profiles?</strong><br />
+                        <span style="font-weight: bold;">4. What will happen if we fake our profiles?</span><br />
                         - Faking of profiles or inaccurate CV and profiles are automatically banned from applying as an Independent Contractor.<br />
                         <br />
-                        <strong>5. What time will we be working?</strong><br />
+                        <span style="font-weight: bold;">5. What time will we be working?</span><br />
                         Depending on the client's preferred working hours or their timezones.<br />
                         <br />
-                        <strong>6. How long will it take to get hired?</strong><br />
+                        <span style="font-weight: bold;">6. How long will it take to get hired?</span><br />
                         - There is no exact answer to this, we are continuously advertising, selling and marketing our services.<br />
                         <br />
-                        <strong>7. Are we working as a regular employee?</strong><br />
-                        - You will be working as an Independent Contractor <a target="_blank" hreef="https://bit.ly/30XkgIX">https://bit.ly/30XkgIX</a><br />
+                        <span style="font-weight: bold;">7. Are we working as a regular employee?</span><br />
+                        - You will be working as an Independent Contractor<br />
                         <br />
-                        <strong>8. Are we working for Draftsource Virtual?</strong><br />
+                        <span style="font-weight: bold;">8. Are we working for Draftsource Virtual?</span><br />
                         - You will be under our company but you will report directly to your clients and prospective customers.<br />
                         - We are a Recruitment and Staffing company not a CAD Outsourcing Company<br />
                         <br />
@@ -178,8 +174,8 @@ router.post('/signup', async (req, res) => {
                         - This is an application as an independent contractor.<br />
                         - Job opportunities will be presented if you are qualified and accepted as we have strict qualifications and a non-disclosure agreement.<br />
                         <br />
-                        <strong>James 1:8-9</strong>
-                        “ 8 Keep this Book of the Law always on your lips; meditate on it day and night, so that you may be careful to do everything written in it. Then you will be <strong>prosperous and successful.</strong><br />
+                        <span style="font-weight: bold;">James 1:8-9</span>
+                        “ 8 Keep this Book of the Law always on your lips; meditate on it day and night, so that you may be careful to do everything written in it. Then you will be <span style="font-weight: bold;">prosperous and successful.</span><br />
                         <br /><br />
                         9 Have I not commanded you? Be strong and courageous. Do not be afraid; do not be discouraged, for the Lord your God will be with you wherever you go.”Create a Resume Access<br />
                         <br /><br />
@@ -189,7 +185,7 @@ router.post('/signup', async (req, res) => {
 				while (!isSuccess) {
 					try {
 						await sendEmail(
-							'Draftsource BPO <do-not-reply@draftsourcebpo.com>',
+							'Draftsource Virtual <do-not-reply@draftsourcebpo.com>',
 							email,
 							'Verify Email',
 							compose
